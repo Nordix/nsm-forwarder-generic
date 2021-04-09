e9654c02f86b6d8fdb55220ec2ef2db0872d4561
Lugossy Zoltan <zoltan.lugossy@est.tech>
2021-04-09 13:07:03 +0200

    generic forwarder; added support for forwarder selection

--- cmd/nsm-forwarder-generic/main.go
+++ cmd/nsm-forwarder-generic/main.go
@@ -66,10 +66,11 @@ import (
 
 // Config - configuration for cmd-forwarder-vpp
 type Config struct {
-	Name             string        `default:"forwarder" desc:"Name of Endpoint"`
-	NSName           string        `default:"xconnectns" desc:"Name of Network Service to Register with Registry"`
-	ConnectTo        url.URL       `default:"unix:///connect.to.socket" desc:"url to connect to" split_words:"true"`
-	MaxTokenLifetime time.Duration `default:"24h" desc:"maximum lifetime of tokens" split_words:"true"`
+	Name             string            `default:"forwarder" desc:"Name of Endpoint"`
+	NSName           string            `default:"xconnectns" desc:"Name of Network Service to Register with Registry"`
+	ConnectTo        url.URL           `default:"unix:///connect.to.socket" desc:"url to connect to" split_words:"true"`
+	MaxTokenLifetime time.Duration     `default:"24h" desc:"maximum lifetime of tokens" split_words:"true"`
+	Labels           map[string]string `default:"" desc:"Endpoint labels"`
 }
 
 var version = "unknown"
@@ -176,8 +177,13 @@ func main() {
 	_, err = registryClient.Register(ctx, &registryapi.NetworkServiceEndpoint{
 		Name:                config.Name,
 		NetworkServiceNames: []string{config.NSName},
-		Url:                 listenOn.String(),
-		ExpirationTime:      expireTime,
+		NetworkServiceLabels: map[string]*registryapi.NetworkServiceLabels{
+			config.NSName: {
+				Labels: config.Labels,
+			},
+		},
+		Url:            listenOn.String(),
+		ExpirationTime: expireTime,
 	})
 	if err != nil {
 		logrus.Fatalf("failed to connect to registry: %+v", err)
@@ -225,8 +231,8 @@ func (s *calloutServer) Request(
 		if conn.Mechanism == nil {
 			// (can conn.Mechanism ever be nil?)
 			conn.Mechanism = &networkservice.Mechanism{
-				Cls:  cls.REMOTE,
-				Type: kernel.MECHANISM,
+				Cls:        cls.REMOTE,
+				Type:       kernel.MECHANISM,
 				Parameters: map[string]string{},
 			}
 		}
@@ -248,6 +254,7 @@ func (s *calloutServer) Close(
 	logrus.Infof("calloutServer(%s); CLOSE", s.id)
 	return next.Server(ctx).Close(ctx, conn)
 }
+
 type mechanismClient struct {
 	id    string
 	mutex sync.Mutex
